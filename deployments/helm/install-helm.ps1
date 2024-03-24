29818308c9bac5e7748d38257a0c70d80ff8b110
Sharad <sharadit2011@hotmail.com>
2024-03-25 01:10:29 +0530

    implemented helm changes for local kubernetes

---
+++ deployments/helm/install-helm.ps1
@@ -0,0 +1,38 @@
+Param(
+    [parameter(Mandatory=$false)][string]$appName="eshopmicroservice"
+    )
+
+function Install-Chart  {
+    Param([string]$chart, [string]$initialOptions)
+    $options=$initialOptions
+
+    $cmd = "helm install $appName-$chart $chart $options"
+    Write-Host "Helm Command: $cmd" -ForegroundColor Gray
+    Invoke-Expression $cmd
+}
+
+
+Write-Host "Installation using Helm started" -ForegroundColor Green
+
+$infras = ("basketdb", "catalogdb", "orderdb", "distributedcache", "messagebroker")
+$apis = ("catalogapi","discountgrpc", "orderapi", "basketapi", "shoppingweb")
+$gateways = ("yarpgateway")
+
+
+foreach ($infra in $infras) {
+    Write-Host "Installing: $infra" -ForegroundColor Green
+    Write-Host "$infra"
+    Install-Chart $infra
+}
+
+foreach ($api in $apis) {
+    Write-Host "Installing: $api" -ForegroundColor Green
+    Install-Chart $api
+}
+
+foreach ($gateway in $gateways) {
+    Write-Host "Installing: $gateway" -ForegroundColor Green
+    Install-Chart $gateway
+}
+
+Write-Host "helm charts installed." -ForegroundColor Green
